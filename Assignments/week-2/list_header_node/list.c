29018616620387947f3e0426dd64185e3428af0d
Pratham S Gowda <prathampsg2007@gmail.com>
2026-08-15 09:45:42 +0530

    linked list with header node

--- Assignments/week-2/list_header_node/list.c
+++ Assignments/week-2/list_header_node/list.c
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include "list.h"
 
-void init_list(list_t *ptr_list)
-{
-	ptr_list->head_ = (node_t*)malloc(sizeof(node_t));
-	ptr_list->head_->next_ = NULL; 
-}
-
 void deinit_list(list_t *ptr_list)
 {
 	node_t* temp;
@@ -21,6 +15,30 @@ void deinit_list(list_t *ptr_list)
 	}
 
 	ptr_list->head_->next_ = NULL;
+}
+
+void delete(list_t* ptr_list, int key)
+{
+	node_t* prev = ptr_list->head_; 
+	node_t* pres = ptr_list->head_->next_;
+
+	while(pres != NULL && pres->key_ < key)
+	{
+		prev = pres;
+		pres = pres->next_;
+	}
+
+	if(pres != NULL && pres->key_ == key)
+	{
+		prev->next_ = pres->next_;
+		free(pres);
+	}
+}
+
+void init_list(list_t *ptr_list)
+{
+	ptr_list->head_ = (node_t*)malloc(sizeof(node_t));
+	ptr_list->head_->next_ = NULL; 
 }
 
 void disp(list_t *ptr_list)
@@ -50,22 +68,4 @@ void insert(list_t* ptr_list, int key)
 	}
 	prev->next_ = temp;
 	temp->next_ = pres;
-}
-
-void delete(list_t* ptr_list, int key)
-{
-	node_t* prev = ptr_list->head_; 
-	node_t* pres = ptr_list->head_->next_;
-
-	while(pres != NULL && pres->key_ < key)
-	{
-		prev = pres;
-		pres = pres->next_;
-	}
-
-	if(pres != NULL && pres->key_ == key)
-	{
-		prev->next_ = pres->next_;
-		free(pres);
-	}
 }
